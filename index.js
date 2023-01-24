b9e0710951aa70db2cbdaf56d40a292683e35a28
ViktorLumiste <viktor.lumiste@gmail.com>
2023-01-24 13:26:56 +0200

    Public directory use

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const express = require('express')
 const app = express();
 app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'))
 app.get('/user/:username', (req,res)=> {
     let user = req.params.username;
     res.render('index.ejs',{username : user});
